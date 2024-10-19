3397739620fc8bfff403e820d6fad0842d32fc72
Alvaro Castro <arucastro@gmail.com>
2024-10-19 02:31:47 +0200

    Fundamentals Part 1 Values and Variables exercise

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+// Values and Variables
+
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country= "Spain"
+let continent = "Europe"
+let population = 45
+
+console.log(country)
+console.log(continent)
+console.log(population)
