e56246c8085c1de54c039635d1f5590f0ebc189f
Alvaro Castro <arucastro@gmail.com>
2024-10-23 02:37:06 +0200

    Strict mode

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,22 @@
+'use strict'
+
+//Strict MODE
+
+let hasDriversLicense = false
+const passTest = true
+
+//script.js:9 Uncaught ReferenceError: hasDriverLicense is not defined
+if(passTest) hasDriverLicense = true
+//Could not find name 'hasDriverLicense'. Did you mean 'hasDriversLicense'?ts(2570)
+if(passTest) hasDriversLicense = true
+
+if(hasDriversLicense) console.log('I can drive :D')
+
+const interface = 'Audio'
+const private = 234
+//Uncaught SyntaxError: Unexpected strict mode reserved word (at script.js:15:7)
+
+
 // Functions﻿
 // Write a function called describeCountry which takes three parameters: country, population and capitalCity. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'.
 
