ac6bd8f25688a11f87faf1847576e37bc1147c47
Alvaro Castro <arucastro@gmail.com>
2024-10-23 03:18:27 +0200

    Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -44,20 +44,63 @@
 
 // Write a function called describeCountry which takes three parameters: country, population and capitalCity. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'.
 
-function describeCountry(country, population, capitalCity){
+// function describeCountry(country, population, capitalCity){
 
-    const result = `${country} has ${population} million people and its capital city is ${capitalCity}`
+//     const result = `${country} has ${population} million people and its capital city is ${capitalCity}`
 
-    return result
+//     return result
 
-    //or
-    // return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//     //or
+//     // return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// }
+
+// // Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
+
+// console.log(describeCountry('Spain', 45, 'Madrid'))
+// console.log(describeCountry('France', 68.4, 'Paris'))
+// console.log(describeCountry('UK', 68, 'London'))
+
+
+
+
+// Function Declarations vs. Expressions﻿
+
+//Function declaration
+//Function declarations are hoisted, meaning they are available throughout the scope (even before they are defined in the code). This means we can call the function before it is defined in the code.
+function calcAge1(birthYear){
+
+    return 2024 - birthYear
 }
+const age1 = calcAge1(1988)
+console.log(age1)
 
-// Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
+//Function expression, anonymous function
+// Function expressions are not hoisted. This means you cannot call the function before it is defined in the code. It behaves just like any other variable, and the function is only available after the expression is evaluated.
+const calcAge2 = function (birthYear){
+    return 2024 - birthYear
+}
+const age2 = calcAge1(1988)
+console.log(age2)
 
-console.log(describeCountry('Spain', 45, 'Madrid'))
-console.log(describeCountry('France', 68.4, 'Paris'))
-console.log(describeCountry('UK', 68, 'London'))
 
+// The world population is 7900 million people. Create a function declaration called percentageOfWorld1 which receives a population value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population.
 
+// To calculate the percentage, divide the given population value by 7900 and then multiply by 100.
+
+// Call percentageOfWorld1 for 3 populations of countries of your choice, store the results into variables, and log them to the console.
+
+// Create a function expression which does the exact same thing, called percentageOfWolrd2, and also call it with 3 country populations (can be the same populations).
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+ }
+ 
+ const percentageOfWorld2 = function (population) {
+   return (population / 7900) * 100;
+ };
+ 
+ const percSpain = percentageOfWorld1(103);
+ const percFrance = percentageOfWorld1(143);
+ const percUk = percentageOfWorld1(233);
+ 
+ console.log(percSpain, percFrance, percUk);
