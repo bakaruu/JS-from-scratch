71b86c73f75b3be6d13c5acb66e36e32457b5014
Alvaro Castro <arucastro@gmail.com>
2024-10-24 03:01:11 +0200

    challenge #1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -165,3 +165,24 @@ const describePopulation = function(country, population) {
   describePopulation('Portugal', 10);
   describePopulation('China', 1441);
   describePopulation('USA', 332);
+
+
+//challenge #1
+/* Write your code below. Good luck! 🙂 */
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {  // Dolphins must have at least double the score
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {  // Koalas must have at least double the score
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`No team wins...`);
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
