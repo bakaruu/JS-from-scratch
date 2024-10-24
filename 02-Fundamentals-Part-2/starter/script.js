dc6a299c2133a7e8ce3c5ae3be611f67db2b755e
Alvaro Castro <arucastro@gmail.com>
2024-10-24 03:39:24 +0200

    Basic Array Operations (Methods)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -239,7 +239,29 @@ const populations = [12, 14, 33, 22]
 
 console.log(populations.length === 4)
 
-const percentages = [percentageOfWorld1(populations[0]),               percentageOfWorld1(populations[1]),               percentageOfWorld1(populations[2]),               percentageOfWorld1(populations[3])]
+const percentages = [percentageOfWorld1(populations[0]),
+                    percentageOfWorld1(populations[1]), 
+                    percentageOfWorld1(populations[2]), 
+                    percentageOfWorld1(populations[3])]
+
 
 
 console.log(percentages)
+
+
+
+
+// Basic Array Operations (Methods)﻿
+
+
+
+
+// Create an array containing all the neighbouring countries of a country of your choice. Choose a country which has at least 2 or 3 neighbours. Store the array into a variable called neighbours.
+
+// At some point, a new country called 'Utopia' is created in the neighbourhood of your selected country, so add it to the end of the neighbours array.
+
+// Unfortunately, after some time the new country is dissolved, so remove it from the end of the array.
+
+// If the neighbours array does not include the country 'Germany', log to the console: 'Probably not a central european country :D'.
+
+// Change the name of one of your neighbouring countries. To do that, find the index of the country in the neighbours array, and then use that index to change the array at that index position. For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
