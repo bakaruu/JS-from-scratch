09dcbfcedaefdf5772b52ccffc3ea7239531a7e4
Alvaro Castro <arucastro@gmail.com>
2024-10-30 04:06:17 +0100

    Iteration: The for Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -503,3 +503,5 @@ if(john.bmi > mark.bmi){
 
 
 
+// Iteration: The for Loop﻿
+// There are elections in your country! in a small town, there are only 50 voters. Use a for loop to simulate the 50 people voting, by logging a string like this to the console (for numbers 1 to 50): 'Voter number 1 is currently voting'.
