0d8c3afffc36f6eed4dc8b8f89845f676e9f909f
Alvaro Castro <arucastro@gmail.com>
2024-10-30 04:28:07 +0100

    Looping Arrays, Breaking and Continuing

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -524,3 +524,12 @@ for (let i = 1; i <= 10; i++){
 
 for (let voter = 1; voter <= 50; voter++)
     console.log(`Voter number ${voter} is currently voting`);
+
+
+// Looping Arrays, Breaking and Continuing﻿
+
+// Let's bring back the populations array from a previous assignment.
+
+// Use a for loop to compute an array called percentages2 containing the percentages of the world population for the 4 population values. Use the function percentageWOrld1 that you created earlier.
+
+// Confirm that percentages2 contains exactly the same values as the percentages array that we created manually in the previous assignment, and reflect on how much better this solution is.
