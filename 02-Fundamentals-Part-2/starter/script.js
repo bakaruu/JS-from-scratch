c64826c198726fd8ca0a6562b6c4831e1824aba5
Alvaro Castro <arucastro@gmail.com>
2024-10-30 19:16:51 +0100

    The while Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -645,6 +645,14 @@ while ( rep <= 10){
     rep++
 }
 
+
+let dice = Math.trunc(Math.random() * 6) + 1
+console.log(dice)
+
+while(dice !== 6){
+    console.log(`You rolled a ${dic}`)
+}
+
 // Recreate the challenge from the lecture Looping Arrays, Breaking and Continuing, but this time using a while loop (call the array percentages3).
 
 // Reflect on what solution you like better for this task: the for loop or the while loop?
