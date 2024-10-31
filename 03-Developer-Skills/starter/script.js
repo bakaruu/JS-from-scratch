7966c1bbd3da2d41e1f74ab5a43cd1dacf962232
Alvaro Castro <arucastro@gmail.com>
2024-10-31 04:52:40 +0100

    Coding Challenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -67,6 +67,23 @@ console.log(measureKelvin());
 
 
 
+// Coding Challenge #1
+
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with theres temperatures.Given
+
+// Example: [17, 21, 23] will print '... 17C in 1 days ... 21C in 2 days ... 23C in 3 days ...'
+
+// Create a function 'printForecast' which takes in an array 'arr'
+// and logs a string like the above to the console.groupEnd
+
+// Use the problem-solving framework: Understand the problem and break it up into sub-problems
+
+
+// TEST DATA 1:  [17, 21, 23]
+// TEST DATA 2:  [12, 5, -5, 0, 4]
+
+
+
 
 
 
