fda75f8fa2b0266fdf20c5d87e7ea4cdf33ae82a
Alvaro Castro <arucastro@gmail.com>
2024-10-31 20:07:26 +0100

    Guess my number Done, TODO reset high score.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,55 +19,87 @@
 // document.querySelector('.number').textContent = '>?'
 // document.querySelector('.guess').value = 23
 
-const randomNumber = Math.floor(Math.random() * 20) + 1;
+let randomNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+const guessInput = document.querySelector('.guess');
+guessInput.disabled = false;
 
 
 console.log(randomNumber);
+
+
+document.querySelector('.again').addEventListener('click', function () {
+    // location.reload();// to reload everything
+    score = 20
+    randomNumber = Math.floor(Math.random() * 20) + 1
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').textContent = ''
+    document.querySelector('body').style.backgroundColor= '#222'
+    document.querySelector('.number').style.width = '15rem'
+    guessInput.disabled = false;
+
+
+});
 
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
 
-    if (score < 0) {
-        document.querySelector('.message').textContent = 'YOU LOST'
-    }
+
+    // When there is no input
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No number'
 
+        // When the player wins
     } else if (guess === randomNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number !'
         document.querySelector('.number').textContent = randomNumber
+        guessInput.disabled = true;
         highScore = score
+        document.querySelector('.highscore').textContent = highScore
 
+        //too change things from CSS select the whole body and the class
+        document.querySelector('body').style.backgroundColor = '#14802b'
+        document.querySelector('.number').style.width = '30rem'
+
+        //When the number is greater than actual number
     } else if (guess > randomNumber) {
 
         if (score > 1) {
             document.querySelector('.message').textContent = '📈 Too high'
             score--;
             document.querySelector('.score').textContent = score;
-            
+
         } else {
             document.querySelector('.message').textContent = 'YOU LOST'
+            guessInput.disabled = true;
             document.querySelector('.score').textContent = 0;
         }
+        //When the number is lower than actual number
     } else if (guess < randomNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📉 Too low'
             score--;
             document.querySelector('.score').textContent = score;
-            
+
         } else {
             document.querySelector('.message').textContent = 'YOU LOST'
+
+            guessInput.disabled = true;
             document.querySelector('.score').textContent = 0;
         }
+        //When the number is wrong
     } else {
         document.querySelector('.message').textContent = '💀    Wrong number'
     }
 
 })
+
+
 
 
 
