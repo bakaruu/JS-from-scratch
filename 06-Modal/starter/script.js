e301033dcd72170282d6a9287c58f2806c8a96fe
Alvaro Castro <arucastro@gmail.com>
2024-11-01 19:03:54 +0100

    Modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,4 +3,12 @@
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
-const btnsOpenModal = document.querySelector('.open-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal') // only select one of them, to select all, use querySelectorAll
+
+console.log(btnsOpenModal);
+
+for (let index = 0; index < btnsOpenModal.length; index++)
+    console.log(btnsOpenModal[index].textContent);
+
+
+
