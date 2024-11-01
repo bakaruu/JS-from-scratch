3285c699fb3cf4882d20aa73d48a95501c06e1be
Alvaro Castro <arucastro@gmail.com>
2024-11-01 19:34:58 +0100

    Working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,10 +5,27 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal') // only select one of them, to select all, use querySelectorAll
 
-console.log(btnsOpenModal);
 
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+//to Open every modal msg selected
 for (let index = 0; index < btnsOpenModal.length; index++)
-    console.log(btnsOpenModal[index].textContent);
+    btnsOpenModal[index].addEventListener('click', openModal);
+
+
+
+//To close modal msg
+btnCloseModal.addEventListener('click', closeModal)
+//To close modal outside the box
+overlay.addEventListener('click', closeModal)
 
 
 
