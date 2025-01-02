9cb03294f6c683f382b6a8cce17048a987ba74de
Alvaro Castro <arucastro@gmail.com>
2025-01-02 23:20:28 +0100

    Score updated, dice hidden.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,3 +2,20 @@
 
 
 // Starting the damn pig game I think
+
+
+// Selecting elements
+const score0El = document.querySelector('#score--0')
+const score1El = document.getElementById('score--1')//Another option
+const diceEl = document.querySelector('.dice')
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+//hiding the dice.
+diceEl.classList.add('hidden')
+
+
+
+
+
+
