1f4f3abbe5f29226b4b8aa010dee9d3d0b16cedd
Alvaro Castro <arucastro@gmail.com>
2025-01-16 19:53:26 +0100

    Player toggle done.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,6 +5,9 @@
 
 
 // Selecting elements
+
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')//Another option
 const current0El = document.getElementById('current--0')
@@ -15,12 +18,19 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
+
+
+
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 //hiding the dice.
 diceEl.classList.add('hidden')
 
+// player 0 an player 1, an array is perfect cause its positions 0 and 1.
+const scores = [0, 0]
 let currentScore = 0;
+let currentPlayer = 0;
 //Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
@@ -38,10 +48,24 @@ btnRoll.addEventListener('click', function () {
 
     if (dice !== 1) {
         currentScore += dice;
-        current0El.textContent = currentScore //Change later
+
+        //Select the player dynamically
+        document.getElementById(`current--${currentPlayer}`).textContent = currentScore
+
+        //Select the player manually
+        //current0El.textContent = currentScore //Change later
 
     } else {
+        //set the score to 0 before switching.
+        document.getElementById(`current--${currentPlayer}`).textContent = 0
+        currentScore = 0
         //switch to next player
+        currentPlayer = currentPlayer === 0 ? 1 : 0;
+
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
+        
+        
     }
 
 });
