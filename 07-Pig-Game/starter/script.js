75d9077121a77fc357b584b4b0b8625f92601700
Alvaro Castro <arucastro@gmail.com>
2025-01-16 19:09:36 +0100

    Checked for rolled 1 done.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,6 +7,8 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')//Another option
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
 const diceEl = document.querySelector('.dice')
 
 const btnNew = document.querySelector('.btn--new')
@@ -18,7 +20,7 @@ score1El.textContent = 0;
 //hiding the dice.
 diceEl.classList.add('hidden')
 
-
+let currentScore = 0;
 //Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
@@ -29,10 +31,18 @@ btnRoll.addEventListener('click', function () {
 
     //2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png` ;
+    diceEl.src = `dice-${dice}.png`;
 
 
-    //3. Check for rolled 1: if true switch to next player
+    //3. Check for rolled 1
+
+    if (dice !== 1) {
+        currentScore += dice;
+        current0El.textContent = currentScore //Change later
+
+    } else {
+        //switch to next player
+    }
 
 });
 
