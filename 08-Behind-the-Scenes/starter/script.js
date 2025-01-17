31662dcafaf1ba493d53e3a48ffa789bca371206
Alvaro Castro <arucastro@gmail.com>
2025-01-17 20:05:19 +0100

    Regular Functions vs. Arrow Functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -141,18 +141,36 @@
 // f()
 
 
+///////////////////////////////////////
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
+
 const jonas = {
     firstName: 'Jonas',
     year: 1991,
     calcAge: function () {
-        console.log(this)
-        console.log(2037 - this.year)
+      // console.log(this);
+      console.log(2037 - this.year);
+  
+      // Solution 1
+      // const self = this; // self or that
+      // const isMillenial = function () {
+      //   console.log(self);
+      //   console.log(self.year >= 1981 && self.year <= 1996);
+      // };
+  
+      // Solution 2
+      const isMillenial = () => {
+        console.log(this);
+        console.log(this.year >= 1981 && this.year <= 1996);
+      };
+      isMillenial();
     },
-
-    //never ever use an arrow function as a method hehe.
-    greet: () => console.log(`Hey ${this.firstName}`),
-
-}
-
-
-jonas.greet()
+  
+    greet: () => {
+      console.log(this);
+      console.log(`Hey ${this.firstName}`);
+    },
+  };
+  jonas.greet();
+  jonas.calcAge();
