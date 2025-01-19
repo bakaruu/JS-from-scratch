f61971307c63960e28f7dc31d1068d6c43e99d8e
Alvaro Castro <arucastro@gmail.com>
2025-01-19 19:56:35 +0100

    Destructuring Arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -35,6 +35,15 @@ const a = arr[0]
 const b = arr[1]
 const c = arr[2]
 
+//this here is destructuring an array.
 const [x, y, z] = arr
 console.log(x, y, z)
 
+
+// const [first, second] = restaurant.categories
+// console.log(first, second)
+
+
+//if we want to take just 1st and 3rd do this.
+const [first, , second] = restaurant.categories
+console.log(first, second)
