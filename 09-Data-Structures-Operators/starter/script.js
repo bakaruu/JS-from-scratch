71eba0e9dd784b0817f9a684d005f0a3192d4e93
Alvaro Castro <arucastro@gmail.com>
2025-01-28 18:20:27 +0100

    Nullish coalescing operator ??

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -55,40 +55,51 @@ const restaurant = {
   },
 };
 
-// SHORT CIRCUITING
-
-// Use ANY data type, return ANY data type, short-circuiting
-
-console.log('------ OR ------')
-console.log(2 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-
-//from left to right return first truthy value
-console.log(undefined || 0  || '' || 'Hello' || 23 || null);
-
-//numGuest going to be 23 if we declare it otherwise 10
-// restaurant.numGuest = 23; //if this is 0, wont work
-const guest1 = restaurant.numGuest ? restaurant.numGuest : 10;
-console.log(guest1)
-
+//Nullish coalescing
+// restaurant.numGuest = 0;
 const guest2 = restaurant.numGuest || 10;
 console.log(guest2)
 
-console.log('------ AND ------');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
+
+//Nullish: null and undefined  (NOT - or '')
+const guestCorrect = restaurant.numGuest ?? 10;
+console.log(guestCorrect)
 
 
-console.log('Hello' && 23 && null && 'Jonas');
+// // SHORT CIRCUITING
 
-//practical example
-if(restaurant.orderPizza){
-  restaurant.orderPizza('mushrooms', 'spinach')
-}
+// // Use ANY data type, return ANY data type, short-circuiting
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach') //hard to read.
+// console.log('------ OR ------')
+// console.log(2 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// //from left to right return first truthy value
+// console.log(undefined || 0  || '' || 'Hello' || 23 || null);
+
+// //numGuest going to be 23 if we declare it otherwise 10
+// // restaurant.numGuest = 23; //if this is 0, wont work
+// const guest1 = restaurant.numGuest ? restaurant.numGuest : 10;
+// console.log(guest1)
+
+// const guest2 = restaurant.numGuest || 10;
+// console.log(guest2)
+
+// console.log('------ AND ------');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+
+// console.log('Hello' && 23 && null && 'Jonas');
+
+// //practical example
+// if(restaurant.orderPizza){
+//   restaurant.orderPizza('mushrooms', 'spinach')
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach') //hard to read.
 
 
 
