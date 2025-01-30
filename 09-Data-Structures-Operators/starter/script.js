ee564fe9076ec02dc6db0977065ce26bd6668974
Alvaro Castro <arucastro@gmail.com>
2025-01-30 20:15:10 +0100

    CHALLENGE #2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -93,32 +93,134 @@ const restaurant = {
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 
-// Looping Objects: Object Keys, Values, and Entries
-const properties = Object.keys(openingHours);
-console.log(properties);
-let openStr = (`We are open on ${properties.length} days: `)
+// Coding Challenge #2
 
-// for (const day of Object.keys(openingHours)) {
-//   console.log(day)
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+  date: 'Nov 9th, 2037',
+
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Let's continue with our football betting app! Keep using the 'game' variable from
+// before.
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
+for ( const [index, player] of game.scored.entries()){
+  console.log(`Goal ${index + 1}: ${player}`)
+}
+
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+const odds = Object.values(game.odds); // we get an array with the values.
+console.log(odds);
+
+let sum = 0;
+for (const odd of odds){
+  sum += odd;
+}
+
+const average = sum / odds.length;
+console.log(average);
+
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the same property names �
+
+for (const [key, value] of Object.entries(game.odds)) {
+  const teamName = key === "x" ? "draw" : `victory ${game[key]}`;
+  
+  console.log(`Odd of ${teamName}: ${value}`);
+}
+
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+// {
+//  Gnarby: 1,
+//  Hummels: 1,
+//  Lewandowski: 2
 // }
+// GOOD LUCK �
 
-for (const day of properties) {
-  openStr += `${day}, `
+const scorers = {};
+
+for (const player of game.scored){
+  scorers[player] = (scorers[player] || 0) +1;
 }
-console.log(openStr);
 
-//Propery VALUES
+console.log(scorers);
 
-const values = Object.values(openingHours);
-console.log(values);
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 
-//Entire object
-const entries = Object.entries(openingHours);
-// console.log(entries);
+// Looping Objects: Object Keys, Values, and Entries
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+// let openStr = (`We are open on ${properties.length} days: `)
 
-for ( const [key, {open, close}] of entries ){
-  console.log(`On ${key} we open at ${open} and close at ${close}`)
-}
+// // for (const day of Object.keys(openingHours)) {
+// //   console.log(day)
+// // }
+
+// for (const day of properties) {
+//   openStr += `${day}, `
+// }
+// console.log(openStr);
+
+// //Propery VALUES
+
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// //Entire object
+// const entries = Object.entries(openingHours);
+// // console.log(entries);
+
+// for ( const [key, {open, close}] of entries ){
+//   console.log(`On ${key} we open at ${open} and close at ${close}`)
+// }
 
 
 ////////////////////////////////////////////////////
