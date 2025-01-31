9bac18bbfe94b31cf9777963b3473fffb158d74b
Alvaro Castro <arucastro@gmail.com>
2025-01-31 18:45:19 +0100

    Working with Strings part1.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -92,103 +92,153 @@ const restaurant = {
   //   },
   // },
 };
-
-
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
+// Working with Strings part1.
 
-// Coding Challenge #3
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-    'Hummels'],
-  date: 'Nov 9th, 2037',
-
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-
-const gameEvents = new Map([
-  [17, '⚽ GOAL'],
-  [36, '♻ Substitution'],
-  [47, '⚽ GOAL'],
-  [61, '♻ Substitution'],
-  [64, '🟨 Yellow Card'],
-  [69, '🟥 Red Card'],
-  [70, '♻ Substitution'],
-  [72, '♻ Substitution'],
-  [76, '⚽ GOAL'],
-  [80, '⚽ GOAL'],
-  [92, '🟨 Yellow Card'],
-]);
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
 
 
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B868'[0]);
 
-// Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+console.log(airline.length);
+console.log('B868'.length);
 
-// Your tasks:
+console.log(airline.indexOf('r')); 
+console.log(airline.lastIndexOf('r')); 
 
-// 1. Create an array 'events' of the different game events that happened (no duplicates)
+//search entire word case sensitive
+console.log(airline.lastIndexOf('portugal')); 
 
-const events = [ ...new Set(gameEvents.values())];
-console.log(events);
+console.log(airline.slice(4)); 
+console.log(airline.slice(4, 7)); 
 
-// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+//when we dont know what string is
 
-gameEvents.delete(64);
-console.log(gameEvents);
+console.log(airline.slice(0, airline.indexOf(' ')))
+console.log(airline.slice(airline.lastIndexOf(' ') + 1))
 
-
-// 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-
-
-const time = [...gameEvents.keys()].pop();
-
-console.log(`An event happened, on average, every ${time / gameEvents.size} minutes.`);
+console.log(airline.slice(-2)); 
+console.log(airline.slice(1, -1)); 
 
 
-// 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+const checkMiddleSeat = function(seat){
+  //B AND E are middle seats.
+  const s = seat.slice(-1);
+  if(s === 'B' || s === 'E')
+    console.log('You got the middle seat.');
+  else console.log('You got lucky');
 
-// [FIRST HALF] 17: ⚽ GOAL
-// GOOD LUCK �
-
-for (const [minute, event] of gameEvents) {
-  const half = minute <= 45 ? 'FIRST HALF' : 'SECOND HALF';
-  console.log(`[${half}] ${minute}: ${event}`);
 }
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+
+
+
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+// Coding Challenge #3
+
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+//     'Hummels'],
+//   date: 'Nov 9th, 2037',
+
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const gameEvents = new Map([
+//   [17, '⚽ GOAL'],
+//   [36, '♻ Substitution'],
+//   [47, '⚽ GOAL'],
+//   [61, '♻ Substitution'],
+//   [64, '🟨 Yellow Card'],
+//   [69, '🟥 Red Card'],
+//   [70, '♻ Substitution'],
+//   [72, '♻ Substitution'],
+//   [76, '⚽ GOAL'],
+//   [80, '⚽ GOAL'],
+//   [92, '🟨 Yellow Card'],
+// ]);
+
+
+
+// // Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+// // Your tasks:
+
+// // 1. Create an array 'events' of the different game events that happened (no duplicates)
+
+// const events = [ ...new Set(gameEvents.values())];
+// console.log(events);
+
+// // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+
+// // 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+
+// const time = [...gameEvents.keys()].pop();
+
+// console.log(`An event happened, on average, every ${time / gameEvents.size} minutes.`);
+
+
+// // 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+
+// // [FIRST HALF] 17: ⚽ GOAL
+// // GOOD LUCK �
+
+// for (const [minute, event] of gameEvents) {
+//   const half = minute <= 45 ? 'FIRST HALF' : 'SECOND HALF';
+//   console.log(`[${half}] ${minute}: ${event}`);
+// }
 
 
 
